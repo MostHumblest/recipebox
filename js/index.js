7bb127285cded281d9ec3047464c5e3be2ae268a
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2020-12-24 06:13:38 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-	alert("ready")
+	alert("ready1")
 	$("button").click(function(){
-		$.getJSON('js/demo_recipe.json', function(data){
+		$.getJSON('/js/demo_recipe.json', function(data){
 			//this gets the json and save it as data
 		});
 		alert(data.title);
