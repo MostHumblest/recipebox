c40f8ecb9a403135790248a351c542a39c0c6920
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2020-12-24 06:15:33 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 	alert("ready1")
 	$("button").click(function(){
-		//$.getJSON('/js/demo_recipe.json', function(data){
-			//this gets the json and save it as data
-		//});
+		$.getJSON('demo_recipe.json', function(recipe){
+			this gets the json and save it as data
+		});
 		alert(recipe.title);
 	});
 });
