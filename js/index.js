5285be9f27e3a7f1e8d27f2b2fbefb60a645ec6e
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2020-12-26 21:54:16 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$("button").click(function(){
 		$("#rname").text(recipes.A1.title);
 		$("#rdiff").text(recipes.A1.difficulty);
-		$("#ring").text(recipes.A1.ingredients);
+		$("#ring").text(recipes.A1.ingredients[1].item);
 	});
 });
 	
