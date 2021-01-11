d4b351bb9fba97c9620c5f5cc36f323e8cce23df
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2021-01-10 19:07:09 -0500

    tested updates

--- js/demo_recipe.js
+++ js/demo_recipe.js
@@ -1,7 +1,8 @@
-recipes = { 
+recipes = [ 
 
-"A1": {
-
+{
+	"id": "A1"
+	
     "title": "Tasty Steak",
 
     "total_cook_time": {
@@ -82,5 +83,90 @@ recipes = {
 
     ]
 
+},
+
+{
+	"id": "A2"
+	
+    "title": "Less Tasty Steak",
+
+    "total_cook_time": {
+
+        "end_to_end": 100000,
+
+        "active": 10000
+
+    },
+
+    "servings_yield": 5,
+
+    "difficulty": "easy",
+
+    "rating": 4.5,
+
+    "source": {
+
+        "url": "https://foodnetwork.com/etc/blarg",
+
+        "title": "Foodnetwork",
+
+        "author": "Foodie McFoodFace"
+
+    },
+
+    "ingredients": [
+
+        {
+
+            "item": "seasoning salt",
+
+            "quantity": "1 teaspoon",
+
+            "notes": "add more for seasoning to taste"
+
+        },
+
+        {
+
+            "item": "all-purpose flour",
+
+            "quantity": "1 cup",
+
+            "notes": ""
+
+        },
+
+        {
+
+            "item": "rib eye steak",
+
+            "quantity": 1,
+
+            "notes": "can substitute a NY strip"
+
+        }
+
+    ],
+
+    "directions": [
+
+        "Salt and pepper both sides of the steak.",
+
+        "Deep fry that sumbitch at 1000 degrees.",
+
+        "Serve with A1 steaksauce."
+
+    ],
+
+    "tags": [
+
+        "meaty",
+
+        "salty",
+
+        "quick"
+
+    ]
+
 }
-}
+]
--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
-	//alert("ready")
+	alert("ready")
 	$("button").click(function(){
 		$("#rname").text(recipes.A1.title);
 		$("#rdiff").text(recipes.A1.difficulty);
-		$.each(recipes.A1.ingredients, function(){
-				var html ="<li>" + this.quantity + " " + this.item + "</li>";
+		$.each(recipes, function(i, datapt){
+				var html ="<li>" + datapt.quantity + " " + datapt.item + "</li>";
 				$("#ring").append(html);
 		});
+	});
+	$.each(recipes, function(i, item){
+		var testText = "<li>" + recipes[i].title + "</li>";
+		$("#titles").append(testText);
 	});
 });
 	
