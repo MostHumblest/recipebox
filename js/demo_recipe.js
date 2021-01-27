8063600a7e4e73d8998917a949e2f9b8d1fb48ba
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2021-01-27 17:26:10 -0500

    Update demo_recipe.js

--- js/demo_recipe.js
+++ js/demo_recipe.js
@@ -1,11 +1,134 @@
 recipes = [ 
 
+{ "id": "demo", "recipe:template",
+	"category":"demo"
+	"time":{
+		"active": 00,
+		"total":00
+	},
+	"source":{
+		"title": "recipebox",
+		"url":"na",
+		"modify":"y"
+	},
+	"ingredients":[
+		{ 
+			"item": "item 1",
+			"quantity": "1 cup"
+		},
+		{	"item": "item 2",
+			"quantity": "1 tsp"
+		}
+	],
+	"directions":[
+		"step 1",
+		"step 2",
+		"step 3"
+	],
+	"notes":[
+		"note 1",
+		"note 2"
+	],
+	"tags":[
+		"test",
+		"test2"
+	],
+	
+		
+		
+	"servings":1,
+	"
+
 {	"id": "A1", "title": "Tasty Steak",
 
 	"category" : "beef",
 
     "total_cook_time": {
 
+        "end_to_end": 100000,
+
+        "active": 10000
+
+    },
+
+    "servings_yield": 5,
+
+    "difficulty": "easy",
+
+    "rating": 4.5,
+
+    "source": {
+
+        "url": "https://foodnetwork.com/etc/blarg",
+
+        "title": "Foodnetwork",
+
+        "author": "Foodie McFoodFace"
+
+    },
+
+    "ingredients": [
+
+        {
+
+            "item": "seasoning salt",
+
+            "quantity": "1 teaspoon",
+
+            "notes": "add more for seasoning to taste"
+
+        },
+
+        {
+
+            "item": "all-purpose flour",
+
+            "quantity": "1 cup",
+
+            "notes": ""
+
+        },
+
+        {
+
+            "item": "rib eye steak",
+
+            "quantity": 1,
+
+            "notes": "can substitute a NY strip"
+
+        }
+
+    ],
+
+    "directions": [
+
+        "Salt and pepper both sides of the steak.",
+
+        "Deep fry that sumbitch at 1000 degrees.",
+
+        "Serve with A1 steaksauce."
+
+    ],
+
+    "tags": [
+
+        "meaty",
+
+        "salty",
+
+        "quick"
+
+    ]
+
+},
+
+{	"id": "A2", "title": "Less Tasty Steak",
+
+	"category" : "chicken",
+
+    "total_cook_time": {
+ 
         "end_to_end": 100000,
 
         "active": 10000
@@ -66,93 +189,9 @@ recipes = [
 
         "Salt and pepper both sides of the steak.",
 
-        "Deep fry that sumbitch at flaming 1000 degrees.",
+        "Deep fry that sumbitch at 1000 degrees.",
 
         "Serve with A1 steaksauce."
-
-    ],
-
-    "tags": [
-
-        "meaty",
-
-        "salty",
-
-        "quick"
-
-    ]
-
-},
-
-{	"id": "A2", "title": "Less Tasty Steak",
-
-	"category" : "chicken",
-
-    "total_cook_time": {
-
-        "end_to_end": 100000,
-
-        "active": 10000
-
-    },
-
-    "servings_yield": 5,
-
-    "difficulty": "easy",
-
-    "rating": 4.5,
-
-    "source": {
-
-        "url": "https://foodnetwork.com/etc/blarg",
-
-        "title": "Foodnetwork",
-
-        "author": "Foodie McFoodFace"
-
-    },
-
-    "ingredients": [
-
-        {
-
-            "item": "seasoning salt",
-
-            "quantity": "1 tsp",
-
-            "notes": "add more for seasoning to taste"
-
-        },
-
-        {
-
-            "item": "all-purpose flour",
-
-            "quantity": "1/2 cup",
-
-            "notes": ""
-
-        },
-
-        {
-
-            "item": "strip steak",
-
-            "quantity": 1,
-
-            "notes": "can substitute a NY strip"
-
-        }
-
-    ],
-
-    "directions": [
-
-        "Salt and pepper one side of the steak.",
-
-        "Deep fry that sumbitch at a measly 900 degrees.",
-
-        "Serve with A2 steaksauce."
 
     ],
 
