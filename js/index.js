d247542b108fd319a6820e1327eacb1ff416b9be
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2021-01-27 21:26:27 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
 		$.each(recipes, function(i, val){
 			var rCat = recipes[i].category;
 			//rCat = rCat.replace('', 'uncategorized');
-			var categoryHeaders = "<h2 id=\"h" + rCat + "\" class=\"headCat\">" + rCat + "<i class=\"arrow right\"></i></h2>";
+			var categoryHeaders = "<h2 id=\"h" + rCat + "\" class=\"headCat\">" + rCat + "<i class=\"right\"></i></h2>";
 			var categoryLists = "<ul id=\"r"+ rCat +"\" class=\"rList\"></ul>";
 			$("#allRecipes").append(categoryHeaders);
 			$("#allRecipes").append(categoryLists);
