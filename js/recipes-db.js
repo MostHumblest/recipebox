fcd4b95aa3ddd792ee66ff4204a3261d9e47bb8e
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2021-02-06 17:12:16 -0500

    new recipes file

--- js/recipes-db.js
+++ js/recipes-db.js
@@ -1,1 +1,41 @@
+recipes = [ 
 
+    {   "id": "demo", "title": "template",
+        "category":"demo",
+        "servings":1,
+        "time":{
+            "active": 00,
+            "total":00
+        },
+        "source":{
+            "title":"recipebox",
+            "author":"steve",
+            "source":"na",
+            "modify":"y"
+        },
+        "ingredients":[
+            { 
+                "item": "item 1",
+                "quantity": "1 cup"
+            },
+            {	"item": "item 2",
+                "quantity": "1 tsp"
+            }
+        ],
+        "directions":[
+            "step 1",
+            "step 2",
+            "step 3"
+        ],
+        "notes":[
+            "note 1",
+            "note 2"
+        ],
+        "tags":[
+            "test",
+            "test2"
+        ],
+    },	
+    
+    ]
+    
