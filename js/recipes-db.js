ab3fa72dece2391dfc4d0b7dc1f9f0c0ff3f78dd
MostHumblest <69197554+MostHumblest@users.noreply.github.com>
2021-02-06 18:23:45 -0500

    sourdough starter

--- js/recipes-db.js
+++ js/recipes-db.js
@@ -36,6 +36,91 @@ recipes = [
             "test2"
         ],
     },	
-    
+
+    {   "id": "A1", "title": "Chopping Sizes",
+        "category":"Tips and Techniques",
+        "servings":0,
+        "time":{
+            "active": 00,
+            "total":00
+        },
+        "source":{
+            "title":"Recipe",
+            "author":"Steve",
+            "source":"multiple",
+            "modify":"n"
+        },
+        "ingredients":[
+            { 
+                "item": "item 1",
+                "quantity": "1 cup"
+            },
+            {	"item": "item 2",
+                "quantity": "1 tsp"
+            }
+        ],
+        "directions":[
+            "step 1",
+            "step 2",
+            "step 3"
+        ],
+        "notes":[
+            "note 1",
+            "note 2"
+        ],
+        "tags":[
+            "test",
+            "test2"
+        ],
+    },	
+
+    {   "id": "O1", "title": "Sourdough Starter Maintenance",
+        "category":"Baking",
+        "servings":"NA",
+        "time":{
+            "active": 00,
+            "total":00
+        },
+        "source":{
+            "title":"Sourdough Starter",
+            "author":"ukn",
+            "source":"America's Test Kitchen",
+            "modify":"y"
+        },
+        "ingredients":[
+            { 
+                "item": "Mature Starter",
+                "quantity": "AR"
+            },
+            {	"item": "AP Flour",
+                "quantity": "AR"
+            },
+            {   "item": "Water",
+                "quantity": "AR"
+            }
+        ],
+        "directions":[
+            "To store and Maintain:",
+            "Measure out 1/4c (2oz) starter and transfer to a clean bowl or container. Add 1/2c (2.5oz) AP Flour and 1/4c (2oz) room-temperature water. Discard remaining starter",
+            "Stir until no dry flour remains",
+            "Cover loosely with plastic, or inverted mason jar lid and let sit at room temp for 5 hours",
+            "Cover tightly and return to fridge, repeat eery 1-2weeks",
+            "To prepare for Baking:",
+            "Measure out 1/2c (4oz) starter and transfer to clean bowl. Add 1c (5oz) AP flour and 1/2c (4oz) room-temperature water",
+            "Stir until no dry flour remains",
+            "Cover and let sit at room temperature for 5 hours",
+            "Measure out amount of starter required for recipe and transfer to second bowl. Cover and refrigerate for at least 12 hours or up to 18 hours",
+            "Follow step 1 to maintain remaining starter"
+        ],
+        "notes":[
+            "For maintaining follow steps 1-4 and return to fridge",
+            "For baking, follow steps 5-9 to increase amount of starterm and use that to provide required starter for your recipe. Maintain the rest per 1-4"
+        ],
+        "tags":[
+            "test",
+            "test2"
+        ],
+    },	
+
     ]
     
